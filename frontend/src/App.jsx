cd118b5c0a10ce38dcb6dff946f79004fcb66375
Satinder Singh <92389558+Rednitas10@users.noreply.github.com>
2025-07-08 06:28:29 -0700

    Add user management placeholder pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,11 @@ import EventReview from './pages/EventReview'
 import EventScreen from './pages/EventScreen'
 import EventScrub from './pages/EventScrub'
 import EventViewAll from './pages/EventViewAll'
+import UsersViewAll from './pages/UsersViewAll'
+import UserAdd from './pages/UserAdd'
+import UserEdit from './pages/UserEdit'
+import UserDelete from './pages/UserDelete'
+import UserLogout from './pages/UserLogout'
 
 function App() {
   const auth = { admin: true, uploader: true, reviewer: true, username: 'demo' }
@@ -28,6 +33,12 @@ function App() {
           <Route path="/events/screen" element={<EventScreen />} />
           <Route path="/events/scrub" element={<EventScrub />} />
           <Route path="/events/viewAll" element={<EventViewAll />} />
+          <Route path="/users/viewAll" element={<UsersViewAll />} />
+          <Route path="/users/add" element={<UserAdd />} />
+          <Route path="/users/edit" element={<UserEdit />} />
+          <Route path="/users/delete" element={<UserDelete />} />
+          <Route path="/users/logout" element={<UserLogout />} />
+          <Route path="/logout" element={<UserLogout />} />
         </Routes>
       </BaseLayout>
     </Router>
